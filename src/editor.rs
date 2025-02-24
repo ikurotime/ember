2e02510de7097fd3d5d399b9e8e3468c0c0cb365
David <davidhuertasortiz@gmail.com>
2025-02-24 01:05:30 +0100

    Initial commit

---
+++ src/editor.rs
@@ -0,0 +1,33 @@
+use crossterm::{
+    event::{read, Event::Key, KeyCode::Char},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn new() -> Editor {
+        Editor {}
+    }
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
+                        _ => (),
+                    }
+                }
+                Err(err) => println!("Error: {}", err),
+                _ => (),
+            }
+        }
+        disable_raw_mode().unwrap();
+    }
+}
